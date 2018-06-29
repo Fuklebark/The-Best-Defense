e487b5ce90264ef61a60c003475d7c1aa3248114
Fuklebark <40508768+Fuklebark@users.noreply.github.com>
2018-06-29 14:45:29 -0400

    Add files via upload

---
+++ TorpedoType Patch.cs
@@ -0,0 +1,33 @@
+﻿using Harmony;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheBestDefense
+{
+    [HarmonyPatch(typeof(Vehicle))]
+    [HarmonyPatch("Awake")]
+
+    public class TorpedoTypePatch
+    {
+        public static TorpedoType ConcussionTorpedoType = new TorpedoType()
+        {
+            techType = Main.ConcussionTorpedoTechType,
+            prefab = Main.GetConcussionTorpedoObject()
+        };
+
+        public static TorpedoType NukeTorpedoType = new TorpedoType()
+        {
+            techType = Main.NukeTorpedoTechType,
+            prefab = Main.GetNukeTorpedoObject()
+        };
+
+        static void Postfix(Vehicle __instance)
+        {
+            __instance.torpedoTypes.Add(ConcussionTorpedoType);
+            __instance.torpedoTypes.Add(NukeTorpedoType);
+        }
+    }
+}
