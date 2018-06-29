4030e695b13175a69ab78d7cfeeeb0731ce1e80e
Fuklebark <40508768+Fuklebark@users.noreply.github.com>
2018-06-29 14:44:59 -0400

    Add files via upload

--- Explosions.cs
+++ Explosions.cs
@@ -7,58 +7,38 @@ using System.Text;
 
 namespace TheBestDefense
 {
-    [HarmonyPatch(typeof(SeamothTorpedoWhirlpool))]
-    [HarmonyPatch("TriggerBehaviours")]
-
     public class Explosions : MonoBehaviour
     {
         public float concussionMaxDamage = 300f;
         public float concussionDetonateRadius = 20f;
-        public float nukeMaxDamage = 8000f;
-        public float nukeDetonateRadius = 60f;
+        public float nukeMaxDamage = 4000f;
+        public float nukeDetonateRadius = 50f;
+
+        public static GameObject ConcussionExplodePrefab;
+        public static GameObject NukeExplodePrefab;
+
+        public void AssignPrefabs()
+        {
+            //var crash = (Resources.Load("WorldEntities/Environment/crash") as GameObject).GetComponent<Crash>();
+            var crash = gameObject.GetComponent(typeof(Crash)) as Crash;
+            var seamothtorpedo = gameObject.GetComponent(typeof(SeamothTorpedo)) as SeamothTorpedo;
+
+            ConcussionExplodePrefab = crash.detonateParticlePrefab;
+            NukeExplodePrefab = seamothtorpedo.explosionPrefab;
+        }
+        
         public void ConcussionExplosion()
         {
+            Utils.PlayOneShotPS(ConcussionExplodePrefab, transform.position, transform.rotation, null);
             DamageSystem.RadiusDamage(concussionMaxDamage, transform.position, concussionDetonateRadius, DamageType.Explosive, gameObject);
             gameObject.GetComponent<LiveMixin>().Kill(DamageType.Normal);
         }
 
         public void NukeExplosion()
         {
+            Utils.PlayOneShotPS(NukeExplodePrefab, transform.position, transform.rotation, null);
             DamageSystem.RadiusDamage(nukeMaxDamage, transform.position, nukeDetonateRadius, DamageType.Explosive, gameObject);
             gameObject.GetComponent<LiveMixin>().Kill(DamageType.Normal);
         }
     }
 }
-
-
-/*
- * 
-
- * Crashfish Explosion Code
-private void Detonate()
-	{
-		if (this.detonateParticlePrefab)
-		{
-			global::Utils.PlayOneShotPS(this.detonateParticlePrefab, base.transform.position, base.transform.rotation, null);
-		}
-		DamageSystem.RadiusDamage(this.maxDamage, base.transform.position, this.detonateRadius, DamageType.Explosive, base.gameObject);
-		base.gameObject.GetComponent<LiveMixin>().Kill(DamageType.Normal);
-	}
-
- * Seamoth Electrical Defense Module Explosion Code
-		int num5 = UWE.Utils.OverlapSphereIntoSharedBuffer(base.transform.position, num, -1, QueryTriggerInteraction.UseGlobal);
-		for (int i = 0; i < num5; i++)
-		{
-			Collider collider = UWE.Utils.sharedColliderBuffer[i];
-			GameObject gameObject2 = UWE.Utils.GetEntityRoot(collider.gameObject);
-			if (gameObject2 == null)
-			{
-				gameObject2 = collider.gameObject;
-			}
-			Creature component = gameObject2.GetComponent<Creature>();
-			LiveMixin component2 = gameObject2.GetComponent<LiveMixin>();
-			if (component != null && component2 != null)
-			{
-				component2.TakeDamage(num2, base.transform.position, DamageType.Electrical, base.gameObject);
-
-    */
